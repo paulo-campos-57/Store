247f3d9a641382d17e8b53ddcddb4030ed37b447
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2021-04-26 14:19:27 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,30 @@
+public class Produto {
+
+	private String codProduto;
+	private String nome;
+	private double valor;
+	
+	public String getCodProduto() {
+		return codProduto;
+	}
+	
+	public void setCodProduto(String codProduto) {
+		this.codProduto = codProduto;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+	
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+}
