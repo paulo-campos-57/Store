9d9aab8004474cdab0daff5e5ad5be7a9b753d80
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2021-04-26 14:20:48 -0300

    Create Compra.java

---
+++ Compra.java
@@ -0,0 +1,66 @@
+public class Compra {
+
+	private Produto produtoCompra;
+	private String codCompra;
+	private String notaFiscal;
+	private String codProCompra;
+	private String qtdCompra;
+	private String codForCompra;
+	private String dataCompra;
+	
+	public Produto getProdutoCompra() {
+		return produtoCompra;
+	}
+	
+	public void setProdutoCompra(Produto produtoCompra) {
+		this.produtoCompra = produtoCompra;
+	}
+	
+	public String getCodCompra() {
+		return codCompra;
+	}
+	
+	public void setCodCompra(String codCompra) {
+		this.codCompra = codCompra;
+	}
+	
+	public String getNotaFiscal() {
+		return notaFiscal;
+	}
+	
+	public void setNotaFiscal(String notaFiscal) {
+		this.notaFiscal = notaFiscal;
+	}
+	
+	public String getCodProCompra() {
+		return codProCompra;
+	}
+	
+	public void setCodProCompra(String codProCompra) {
+		this.codProCompra = codProCompra;
+	}
+	
+	public String getQtdCompra() {
+		return qtdCompra;
+	}
+	
+	public void setQtdCompra(String qtdCompra) {
+		this.qtdCompra = qtdCompra;
+	}
+	
+	public String getCodForCompra() {
+		return codForCompra;
+	}
+	
+	public void setCodForCompra(String codForCompra) {
+		this.codForCompra = codForCompra;
+	}
+	
+	public String getDataCompra() {
+		return dataCompra;
+	}
+	
+	public void setDataCompra(String dataCompra) {
+		this.dataCompra = dataCompra;
+	}
+}
