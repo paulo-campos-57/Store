b26a5f1d2496ba3bdc01f29c35e4061e75895fa8
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2021-04-26 14:14:20 -0300

    Create Contato.java

---
+++ Contato.java
@@ -0,0 +1,48 @@
+public class Contato {
+
+	private String tel1;
+	private String tel2;
+	private String cel1;
+	private String cel2;
+	private String email;
+	
+	public String getTel1() {
+		return tel1;
+	}
+	
+	public void setTel1(String tel1) {
+		this.tel1 = tel1;
+	}
+	
+	public String getTel2() {
+		return tel2;
+	}
+	
+	public void setTel2(String tel2) {
+		this.tel2 = tel2;
+	}
+	
+	public String getCel1() {
+		return cel1;
+	}
+	
+	public void setCel1(String cel1) {
+		this.cel1 = cel1;
+	}
+	
+	public String getCel2() {
+		return cel2;
+	}
+	
+	public void setCel2(String cel2) {
+		this.cel2 = cel2;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
