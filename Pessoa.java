853d88d151b707d579c9fc220ee5d8f49e358c56
Paulo Campos <77108503+paulo-campos-57@users.noreply.github.com>
2021-04-26 14:15:58 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,57 @@
+public class Pessoa {
+	
+	private Endereco endereco;
+	private Contato contato;
+	private String codigo;
+	private String nome;
+	private String dataNasc;
+	private String identidade;
+	
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	public Contato getContato() {
+		return contato;
+	}
+	
+	public void setContato(Contato contato) {
+		this.contato = contato;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getDataNasc() {
+		return dataNasc;
+	}
+	
+	public void setDataNasc(String dataNasc) {
+		this.dataNasc = dataNasc;
+	}
+	
+	public String getIdentidade() {
+		return identidade;
+	}
+	
+	public void setIdentidade(String identidade) {
+		this.identidade = identidade;
+	}
+}
